dea636819d2a94d35b8ace90889a36f3580affed
y7chiu <130111605+y7chiu@users.noreply.github.com>
2024-05-02 22:49:17 -0700

    update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,9 +4,23 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-  var option = document.querySelector("[value='select']");
-  console.log(option);
+  //var option = document.querySelector("[value='select']");
+  //console.log(option);
+  const hornSelect = document.getElementById('horn-select');
+  const volume = document.getElementById('volume');
+  const volumeIcon = document.querySelector('.icons img');
+  const soundType = document.getElementById('audio-selection');
+  const playSoundButton = document.getElementById('play-button');
 
-  
+  hornSelect.addEventListener('change', function(){
 
+  });
+
+  volume.addEventListener('input', function(){
+
+  });
+
+  playSoundButton.addEventListener('click', function(){
+
+  });
 }
