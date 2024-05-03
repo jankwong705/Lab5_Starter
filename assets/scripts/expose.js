a01f680b8d8e68c2203ec689223dbea8d671c698
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2024-05-03 16:54:05 -0700

    change image

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -9,11 +9,24 @@ function init() {
   const hornSelect = document.getElementById("horn-select");
   const volume = document.getElementById('volume');
   const volumeIcon = document.querySelector("[alt='Volume level 2']");
+  const image = document.querySelector("[alt='No image selected']");
   // const soundType = document.getElementById('audio-selection');
   const playSoundButton = document.querySelector('button');
 
   hornSelect.addEventListener('change', function(){ //OK
-    alert("hi");
+    switch (hornSelect.value) {
+      case "air-horn":
+        image.src = "assets/images/air-horn.svg";
+        break;
+      case "car-horn":
+        image.src = "assets/images/car-horn.svg";
+        break;
+      case "party-horn":
+        image.src = "assets/images/party-horn.svg";
+        break;
+      default:
+        image.src = "assets/images/no-image.png";
+    }
   });
 
   volume.addEventListener('change', function(){ //OK
