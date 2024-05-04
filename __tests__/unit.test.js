0720fe9a3311843dd5517aebb207b63516ffe051
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2024-05-04 16:10:53 -0700

    unit tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -36,11 +36,11 @@ test('expect jykwong@ucsd.edu true', () => {
   expect(isEmail('jykwong@ucsd.edu')).toBe(true);
 });
 
-test('expect jykwongucsd.edu true', () => {
+test('expect jykwongucsd.edu false', () => {
   expect(isEmail('jykwongucsd.edu')).toBe(false);
 });
 
-test('expect jykwong@ucsdedu true', () => {
+test('expect jykwong@ucsdedu false', () => {
   expect(isEmail('jykwong@ucsdedu')).toBe(false);
 });
 
@@ -53,11 +53,44 @@ test('expect A888 true', () => {
   expect(isStrongPassword('A888')).toBe(true);
 });
 
-test('expect A8 true', () => {
+test('expect A8 false', () => {
   expect(isStrongPassword('A8')).toBe(false);
 });
 
-test('expect Adkjfdsfhduhkdsff8 true', () => {
+test('expect Adkjfdsfhduhkdsff8 false', () => {
   expect(isStrongPassword('Adkjfdsfhduhkdsff8')).toBe(false);
 });
 
+//test isDate
+test('expect 02/02/2002 true', () => {
+  expect(isDate('02/02/2002')).toBe(true);
+});
+
+test('expect 02/12/2012 true', () => {
+  expect(isDate('02/12/2012')).toBe(true);
+});
+
+test('expect 02122012 false', () => {
+  expect(isDate('02122012')).toBe(false);
+});
+
+test('expect 11 false', () => {
+  expect(isDate('11')).toBe(false);
+});
+
+//test isHexColor
+test('expect #3F7CDE true', () => {
+  expect(isHexColor('#3F7CDE')).toBe(true);
+});
+
+test('expect 000 true', () => {
+  expect(isHexColor('000')).toBe(true);
+});
+
+test('expect 11 false', () => {
+  expect(isHexColor('11')).toBe(false);
+});
+
+test('expect #FGH false', () => {
+  expect(isHexColor('#FGH')).toBe(false);
+});
