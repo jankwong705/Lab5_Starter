b3d034a97cb7d3cb89dea265f02643666bde2211
y7chiu <130111605+y7chiu@users.noreply.github.com>
2024-05-03 17:21:04 -0700

    implement the sound icon and add sound in horn select.

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -10,19 +10,22 @@ function init() {
   const volume = document.getElementById('volume');
   const volumeIcon = document.querySelector("[alt='Volume level 2']");
   const image = document.querySelector("[alt='No image selected']");
-  // const soundType = document.getElementById('audio-selection');
+  //const soundType = document.getElementById('audio-selection');
   const playSoundButton = document.querySelector('button');
 
   hornSelect.addEventListener('change', function(){ //OK
     switch (hornSelect.value) {
       case "air-horn":
         image.src = "assets/images/air-horn.svg";
+        audio.src = "assets/audio/air-horn.mp3";
         break;
       case "car-horn":
         image.src = "assets/images/car-horn.svg";
+        audio.src = "assets/audio/car-horn.mp3";
         break;
       case "party-horn":
         image.src = "assets/images/party-horn.svg";
+        audio.src = "assets/audio/party-horn.mp3";
         break;
       default:
         image.src = "assets/images/no-image.png";
@@ -30,7 +33,15 @@ function init() {
   });
 
   volume.addEventListener('change', function(){ //OK
-    alert("hi");
+    if(volume.value == 0){
+      volumeIcon.src = "assets/icons/volume-level-0.svg";
+    } else if(volume.value >= 1 && volume.value < 33){
+      volumeIcon.src = "assets/icons/volume-level-1.svg";
+    } else if(volume.value >= 33 && volume.value < 67){
+      volumeIcon.src = "assets/icons/volume-level-2.svg";
+    } else if(volume.value >= 67){
+      volumeIcon.src = "assets/icons/volume-level-3.svg";
+    }
   });
 
   playSoundButton.addEventListener('click', function(){ //OK
