92d58019443bd3acb9341e945f85a10f0c025561
y7chiu <130111605+y7chiu@users.noreply.github.com>
2024-05-05 16:22:06 -0700

    TheList out, but when clicked, not saying anything

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,54 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-}
+  const synth = window.speechSynthesis;
+
+  const voiceSelect = document.getElementById("voice-select");
+  const talkButton = document.querySelector('button');
+  const smileImg = document.querySelector("[alt='Smiling face']");
+  const text = document.getElementById('text-to-speak');
+
+  let voices = [];
+  function populateVoiceList() {
+    voices = synth.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  const utterThis = new SpeechSynthesisUtterance(text.value);
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute("data-name");
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+
+  smileImg.addEventListener('change', function(){ // Not good
+  if(synth.speaking)
+    smileImg.src = "assets/images/smiling-open.png";
+  else
+    smileImg.src = "assets/images/smiling.png";
+});
+
+  talkButton.addEventListener('click', () => {// Not good
+    synth.speak(utterThis);
+  });
+
+  }
